39acb1534a1b5e475b1bec7185e7eea4eca41c96
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-04-26 13:28:44 +0300

    Initial commit of script.js with variable swapping logic and commented-out user prompts

---
+++ JS/1/script.js
@@ -0,0 +1,13 @@
+// var myName = "ahmad";
+// alert(myName);
+// var yourName = prompt("Whats your name?");
+// alert("My Name is " + myName + " welcome to my course " + yourName + "!");
+
+var a = '3';
+var b = '8';
+var c = a;
+a = b;
+b = c;
+
+console.log('a is ' + a);
+console.log('b is ' + b);
