0a225d3c1203b6b7aadb595239ba5052f22daf12
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-04-30 00:54:10 +0300

    Refactor BMI calculation to use Math.pow for improved readability and accuracy

--- JS/1/BMI_Calculator.js
+++ JS/1/BMI_Calculator.js
@@ -1,7 +1,7 @@
 function calBmi(weight, hight) {
 	var weight = prompt('What is your weight in kg?');
 	var hight = prompt('what is your hight?');
-	var bmi = weight / (hight * hight);
+	var bmi = weight / Math.pow(hight, 2);
 	return bmi;
 }
 
