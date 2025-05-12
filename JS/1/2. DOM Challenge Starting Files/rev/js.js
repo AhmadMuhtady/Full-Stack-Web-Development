f4d34c5af05edad7dbc0b4da38add0131e7d6c03
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-05-12 18:50:30 +0300

    Add initial HTML structure and JavaScript functionality for greeting and random calculations

---
+++ JS/1/2. DOM Challenge Starting Files/rev/js.js
@@ -0,0 +1,21 @@
+// greet = 'hello world';
+// console.log(greet.length);
+// console.log(`${greet.charAt().toUpperCase()}${greet.substring(1)}`);
+
+// let x;
+// let y;
+// x = Math.floor(Math.random() * 100 + 1);
+// y = Math.floor(Math.random() * 50 + 1);
+// sum = `${x} + ${y} = sum`;
+// sub = `${x} - ${y} = sub`;
+// dev = `${x} / ${y} = dev`;
+// times = `${x} * ${y} = times`;
+// mod = `${x} % ${y} = mod`;
+
+// console.log(x);
+// console.log(y);
+// console.log(sum);
+// console.log(sub);
+// console.log(dev);
+// console.log(times);
+// console.log(mod);
