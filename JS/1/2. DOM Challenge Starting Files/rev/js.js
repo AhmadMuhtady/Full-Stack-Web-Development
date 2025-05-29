b4edacd8fa844809d23c1216a725d36d3ef7028e
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-05-29 03:13:38 +0300

    Add FizzBuzz and multiplication table implementations as commented examples

--- JS/1/2. DOM Challenge Starting Files/rev/js.js
+++ JS/1/2. DOM Challenge Starting Files/rev/js.js
@@ -2623,3 +2623,44 @@
 // 		? console.log('ahmad is the best')
 // 		: console.log(names[i]);
 // }
+
+// let i = 1;
+
+// while (i <= 5) {
+// 	console.log('number ' + i);
+// 	let j = 1;
+// 	while (j <= 5) {
+// 		console.log(`${i} * ${j} = ${i * j}`);
+// 		j++;
+// 	}
+// 	i++;
+// }
+
+// do {
+// 	console.log('Number ' + i);
+// 	i++;
+// } while (i <= 10);
+
+// for (i = 1; i <= 50; i++) {
+// 	if (i % 15 === 0) {
+// 		console.log('FizzBuzz');
+// 	} else if (i % 3 === 0) {
+// 		console.log('Fizz');
+// 	} else if (i % 5 === 0) {
+// 		console.log('Buzz');
+// 	} else console.log('Number ' + i);
+// }
+
+// let i = 1;
+
+// while (i <= 50) {
+// 	if (i % 15 === 0) {
+// 		console.log('FizzBuzz');
+// 	} else if (i % 3 === 0) {
+// 		console.log('Fizz');
+// 	} else if (i % 5 === 0) {
+// 		console.log('Buzz');
+// 	} else console.log('Number ' + i);
+
+// 	i++;
+// }
