a37aa18cf72aac6152b61ffa0ac394338513e6ed
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-05-30 05:28:26 +0300

    Add commented examples for array methods and filter functionality

--- JS/1/2. DOM Challenge Starting Files/rev/js.js
+++ JS/1/2. DOM Challenge Starting Files/rev/js.js
@@ -2664,3 +2664,35 @@
 
 // 	i++;
 // }
+
+// const names = ['ahmad', 'omar', 'ali', 'abbas'];
+
+// // for (const name of names) {
+// // 	console.log(name);
+// // }
+
+// names.forEach((name, index, arr) => console.log(`${index} - ${name}`, arr));
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 32];
+
+// const result = numbers.filter((number) => number % 2 === 0);
+// const result = numbers.filter((number) => number > 5);
+// console.log(result);
+// const evenNumbers = [];
+// numbers.forEach((number) => {
+// 	number % 2 === 0 ? evenNumbers.push(number) : null;
+// });
+
+// console.log(evenNumbers);
+
+// const companies = [
+// 	{ name: 'Company One', category: 'Finance', start: 1981, end: 2004 },
+// 	{ name: 'Company Two', category: 'Retail', start: 1992, end: 2008 },
+// 	{ name: 'Company Three', category: 'Auto', start: 1999, end: 2007 },
+// 	{ name: 'Company Four', category: 'Retail', start: 1989, end: 2010 },
+// 	{ name: 'Company Five', category: 'Technology', start: 2009, end: 2014 },
+// 	{ name: 'Company Six', category: 'Finance', start: 1987, end: 2010 },
+// 	{ name: 'Company Seven', category: 'Auto', start: 1986, end: 1996 },
+// 	{ name: 'Company Eight', category: 'Technology', start: 2011, end: 2016 },
+// 	{ name: 'Company Nine', category: 'Retail', start: 1981, end: 1989 },
+// ];
