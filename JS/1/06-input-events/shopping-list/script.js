ac4de386189b2cdd6ad80ef087dc186950e8b7a5
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-06-23 20:02:01 +0300

    Remove commented-out event listener code from shopping list script

---
+++ JS/1/06-input-events/shopping-list/script.js
@@ -0,0 +1,30 @@
+// const itemInput = document.getElementById('item-input');
+// const priorityInput = document.getElementById('priority-input');
+// const checkBox = document.getElementById('checkbox');
+// const heading = document.querySelector('h1');
+
+// const onInput = (e) => {
+// 	heading.textContent = e.target.value;
+// };
+
+// const onChecked = (e) => {
+// 	const isChecked = e.target.checked;
+// 	heading.textContent = isChecked ? 'checked' : 'not checked';
+// };
+
+// const onFocus = () => {
+// 	console.log('input on fucus');
+// 	itemInput.style.outlineStyle = 'solid';
+// 	itemInput.style.outlineWidth = '1px';
+// 	itemInput.style.outlineColor = 'blue';
+// };
+
+// const onBlur = () => {
+// 	console.log('input is not focus');
+// 	itemInput.style.outlineStyle = 'none';
+// };
+// itemInput.addEventListener('input', onInput);
+// priorityInput.addEventListener('change', onInput);
+// checkBox.addEventListener('input', onChecked);
+// itemInput.addEventListener('focus', onFocus);
+// itemInput.addEventListener('blur', onBlur);
