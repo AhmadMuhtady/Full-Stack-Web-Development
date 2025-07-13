aa7d7dec9975c4a73c305f8fcaebab8c50742db5
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-07-13 19:20:10 +0300

    updated js

--- JS/1/09-promise-chaining/script.js
+++ JS/1/09-promise-chaining/script.js
@@ -1,17 +1,25 @@
 const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    let error = false;
+	setTimeout(() => {
+		let error = false;
 
-    if (!error) {
-      resolve({ name: 'John', age: 30 });
-    } else {
-      reject('Error: Something went wrong');
-    }
-  }, 1000);
+		if (!error) {
+			resolve({ name: 'John', age: 30 });
+		} else {
+			reject('Error: Something went wrong');
+		}
+	}, 1000);
 });
 
 promise
-  .then((user) => {
-    console.log(user);
-  })
-  .catch((error) => console.log(error))
+	.then((user) => {
+		console.log(user);
+		return user.name;
+	})
+	.then((name) => {
+		console.log(name);
+		return name.length;
+	})
+	.then((nameLength) => {
+		console.log(nameLength);
+	})
+	.catch((error) => console.log(error));
