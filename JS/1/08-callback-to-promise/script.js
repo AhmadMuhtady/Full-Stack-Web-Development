c93c764ca827fe989aac66e0766b3af2acc53741
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-07-13 20:20:02 +0300

    updated promise

--- JS/1/08-callback-to-promise/script.js
+++ JS/1/08-callback-to-promise/script.js
@@ -3,6 +3,39 @@ const posts = [
 	{ title: 'Post Two', body: 'This is post two' },
 ];
 
+// function createPost(post) {
+// 	return new Promise((resolve, reject) => {
+// 		setTimeout(() => {
+// 			let error = true;
+
+// 			if (!error) {
+// 				posts.push(post);
+// 				resolve();
+// 			} else {
+// 				reject('something went wrong');
+// 			}
+// 		}, 2000);
+// 	});
+// }
+
+// function getPosts() {
+// 	setTimeout(() => {
+// 		posts.forEach(function (post) {
+// 			const div = document.createElement('div');
+// 			div.innerHTML = `<strong>${post.title}</strong> - ${post.body}`;
+// 			document.querySelector('#posts').appendChild(div);
+// 		});
+// 	}, 1000);
+// }
+
+// createPost({ title: 'Post Three', body: 'This is post' })
+// 	.then(getPosts)
+// 	.catch((error) => {
+// 		const h3 = document.createElement('h3');
+// 		h3.innerHTML = `<strong>${error}</strong>`;
+// 		document.getElementById('posts').appendChild(h3);
+// 	});
+
 function createPost(post) {
 	return new Promise((resolve, reject) => {
 		setTimeout(() => {
@@ -12,7 +45,7 @@ function createPost(post) {
 				posts.push(post);
 				resolve();
 			} else {
-				reject('something went wrong');
+				reject('Error Something went wrong');
 			}
 		}, 2000);
 	});
@@ -28,10 +61,11 @@ function getPosts() {
 	}, 1000);
 }
 
-createPost({ title: 'Post Three', body: 'This is post' })
+function showError(error) {
+	const h3 = document.createElement('h3');
+	h3.innerHTML = `<strong>${error}</strong>`;
+	document.getElementById('posts').appendChild(h3);
+}
+createPost({ title: 'Post Three', body: 'This is post Three' })
 	.then(getPosts)
-	.catch((error) => {
-		const h3 = document.createElement('h3');
-		h3.innerHTML = `<strong>${error}</strong>`;
-		document.getElementById('posts').appendChild(h3);
-	});
+	.catch(showError);
