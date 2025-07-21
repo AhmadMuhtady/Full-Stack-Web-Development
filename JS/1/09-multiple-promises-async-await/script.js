e63e53c4ae4464605b9eba3fcbbb787206a831ee
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-07-21 19:12:30 +0300

    Geolocation update

--- JS/1/09-multiple-promises-async-await/script.js
+++ JS/1/09-multiple-promises-async-await/script.js
@@ -1,34 +1,89 @@
-function getData(endpoint) {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', endpoint);
+// function getData(endpoint) {
+// 	return new Promise((resolve, reject) => {
+// 		const xhr = new XMLHttpRequest();
+// 		xhr.open('GET', endpoint);
 
-    xhr.onreadystatechange = function () {
-      if (this.readyState === 4) {
-        if (this.status === 200) {
-          resolve(JSON.parse(this.responseText));
-        } else {
-          reject('Error: Something went wrong');
-        }
-      }
-    };
+// 		xhr.onreadystatechange = function () {
+// 			if (this.readyState === 4) {
+// 				if (this.status === 200) {
+// 					resolve(JSON.parse(this.responseText));
+// 				} else {
+// 					reject('Error: Something went wrong');
+// 				}
+// 			}
+// 		};
 
-    setTimeout(() => {
-      xhr.send();
-    }, Math.floor(Math.random() * 3000) + 1000);
-  });
-}
+// 		setTimeout(() => {
+// 			xhr.send();
+// 		}, Math.floor(Math.random() * 3000) + 1000);
+// 	});
+// }
 
-getData('./movies.json')
-  .then((movies) => {
-    console.log(movies);
-    return getData('./actors.json');
-  })
-  .then((actors) => {
-    console.log(actors);
-    return getData('./directors.json');
-  })
-  .then((directors) => {
-    console.log(directors);
-  })
-  .catch((error) => console.log(error));
+// getData('./movies.json')
+//   .then((movies) => {
+//     console.log(movies);
+//     return getData('./actors.json');
+//   })
+//   .then((actors) => {
+//     console.log(actors);
+//     return getData('./directors.json');
+//   })
+//   .then((directors) => {
+//     console.log(directors);
+//   })
+//   .catch((error) => console.log(error));
+
+// async function getAllData() {
+// 	const movies = await getData('./movies.json');
+
+// 	const actors = await getData('./actors.json');
+
+// 	const directors = await getData('./directors.json');
+// 	console.log(movies, actors, directors);
+// }
+
+// async function getAllDataWithFetch() {
+// 	try {
+// 		const moviesRes = await fetch('./movies.json');
+// 		const moviesData = await moviesRes.json();
+
+// 		const actorsRes = await fetch('./actors.json');
+// 		const actorsData = await actorsRes.json();
+
+// 		const directorsRes = await fetch('./directors.json');
+// 		const directorsData = await directorsRes.json();
+
+// 		console.log(moviesData, actorsData, directorsData);
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
+// }
+
+// getAllDataWithFetch();
+// getAllData();
+
+const getAllDataWithFetch = async () => {
+	try {
+		const [moviesRes, actorsRes, directorsRes] = await Promise.all([
+			fetch('./movies.json'),
+			fetch('./actors.json'),
+			fetch('./directors.json'),
+		]);
+
+		const [moviesData, actorsData, directorsData] = await Promise.all([
+			moviesRes.json(),
+			actorsRes.json(),
+			directorsRes.json(),
+		]);
+
+		console.log({
+			movies: moviesData,
+			actors: actorsData,
+			directors: directorsData,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+getAllDataWithFetch();
