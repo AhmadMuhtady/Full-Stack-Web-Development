54fc93213b70c556beb1f14a24e9a09e39debc0a
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-07-23 19:58:45 +0300

    added const values

---
+++ JS/1/07-audio-api/script.js
@@ -0,0 +1,5 @@
+const audio = document.getElementById('audio');
+const play = document.getElementById('play');
+const pause = document.getElementById('pause');
+const stop = document.getElementById('stop');
+const range = document.getElementById('range');
