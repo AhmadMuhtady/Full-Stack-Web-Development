3b85333944c0ca99ebcda21cbbec64533a761573
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-07-23 20:20:07 +0300

    added eventListeners to btns

--- JS/1/07-audio-api/script.js
+++ JS/1/07-audio-api/script.js
@@ -2,4 +2,23 @@ const audio = document.getElementById('audio');
 const play = document.getElementById('play');
 const pause = document.getElementById('pause');
 const stop = document.getElementById('stop');
-const range = document.getElementById('range');
+const currentTime = document.getElementById('current-time');
+const volume = document.getElementById('volume');
+
+play.addEventListener('click', () => audio.play());
+pause.addEventListener('click', () => audio.pause());
+stop.addEventListener('click', () => {
+	audio.pause();
+	audio.currentTime = 0;
+});
+audio.addEventListener('timeupdate', () => {
+	if (audio.currentTime.toFixed(1) < 60) {
+		currentTime.innerHTML = `${audio.currentTime.toFixed(1)} seconds`;
+	} else {
+		currentTime.innerHTML = `${audio.currentTime.toFixed(1) / 60} minutes`;
+	}
+});
+
+volume.addEventListener('change', () => {
+	audio.volume = volume.value;
+});
