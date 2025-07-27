a6f9903f9f42a6a4423ce3151d613640a58722a1
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-07-27 22:05:57 +0300

    Literals vs Built-In Contructors

--- JS/1/01-object-literals-this/script.js
+++ JS/1/01-object-literals-this/script.js
@@ -11,22 +11,38 @@
 
 // Constructor Functions
 
-function Rectangle(name, width, hight) {
-	(this.name = name),
-		(this.width = width),
-		(this.hight = hight),
-		(this.area = function () {
-			return this.hight * this.width;
-		});
-}
+// function Rectangle(name, width, hight) {
+// 	(this.name = name),
+// 		(this.width = width),
+// 		(this.hight = hight),
+// 		(this.area = function () {
+// 			return this.hight * this.width;
+// 		});
+// }
 
-const rect1 = new Rectangle('rectangle 1', 10, 10);
-const rect3 = new Rectangle('rectangle 3', 20, 10);
-const rect2 = new Rectangle('rectangle 2', 30, 10);
-console.log(rect1);
-console.log(rect1.area());
-console.log(rect3.area());
-console.log(rect2.area());
+// const rect1 = new Rectangle('rectangle 1', 10, 10);
+// const rect3 = new Rectangle('rectangle 3', 20, 10);
+// const rect2 = new Rectangle('rectangle 2', 30, 10);
+// console.log(rect1);
+// console.log(rect1.area());
+// console.log(rect3.area());
+// console.log(rect2.area());
 
-console.log(rect1.constructor);
-console.log(rect2 instanceof Rectangle);
+// console.log(rect1.constructor);
+// console.log(rect2 instanceof Rectangle);
+
+const strList = 'hello';
+const strObj = new String('hello');
+
+console.log(strList, typeof strList);
+console.log(strObj, typeof strObj);
+
+// boxing
+console.log(strList.toUpperCase());
+console.log(strList[0]);
+
+// unboxing
+console.log(typeof strObj.valueOf());
+
+console.log(strList instanceof String);
+console.log(strObj instanceof String);
