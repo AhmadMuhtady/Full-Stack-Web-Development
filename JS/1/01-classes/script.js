7eaa74125451d73d98e10275300e3668ceb23767
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-08-02 03:32:16 +0300

    ES2022 Private Class Fields

--- JS/1/01-classes/script.js
+++ JS/1/01-classes/script.js
@@ -213,3 +213,55 @@
 // console.log(person2.firstName);
 // console.log(person2.lastName);
 // console.log(person2.fullName);
+
+// Private Property Convention hiding info
+
+class Wallet {
+	#balance = 0;
+	#transactions = [];
+
+	deposit(amount) {
+		this.#processDeposit(amount);
+		this.#balance += amount;
+	}
+
+	withdraw(amount) {
+		if (amount > this.#balance) {
+			console.log(`insufficient fund`);
+			return;
+		}
+
+		this.#processWithdraw(amount);
+		this.#balance -= amount;
+	}
+
+	#processDeposit(amount) {
+		console.log(`Depositing ${amount}`);
+		this.#transactions.push({
+			type: 'Deposit',
+			amount,
+		});
+	}
+
+	#processWithdraw(amount) {
+		console.log(`Withdrawing ${amount}`);
+		this.#transactions.push({
+			type: 'Withdraw',
+			amount,
+		});
+	}
+
+	get balance() {
+		return `$${this.#balance}`;
+	}
+
+	get transactions() {
+		return this.#transactions;
+	}
+}
+
+const wallet = new Wallet();
+wallet.deposit(500);
+wallet.withdraw(100);
+console.log(wallet.transactions);
+console.log(wallet.balance);
