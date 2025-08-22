3b25eee5d4960c7d938e643577833479543769bd
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-08-22 23:35:02 +0300

    Maps

--- JS/1/05-sets/script.js
+++ JS/1/05-sets/script.js
@@ -15,3 +15,40 @@
 // }
 // const iterator = set.values();
 // console.log(iterator.next().value);
+
+// Maps
+// const myMap = new Map();
+
+// myMap.set('name', 'John');
+// myMap.set(1, 'blue');
+// myMap.set(2, 'red');
+
+// console.log(myMap.get('name'));
+// console.log(myMap.get(1));
+// console.log(myMap.get(2));
+// console.log(myMap.size);
+// console.log(myMap.has(1));
+// console.log(myMap.has(3));
+// // myMap.delete(2);
+// console.log(myMap);
+
+const peopleMap = new Map();
+peopleMap.set('brad', { phone: '555-555-555', email: 'brad@example.com' });
+peopleMap.set('jack', { phone: '666-666-666', email: 'jack@example.com' });
+peopleMap.set('joe', { phone: '777-777-777', email: 'joe@example.com' });
+peopleMap.set('jill', { phone: '888-888-888', email: 'jill@example.com' });
+
+peopleMap.forEach((person) => console.log(person.email));
+console.log(peopleMap.keys());
+console.log(peopleMap.values());
+console.log(peopleMap.entries());
+
+const iterator = peopleMap.values();
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+
+const peopleArray = Array.from(peopleMap);
+console.log(peopleArray);
