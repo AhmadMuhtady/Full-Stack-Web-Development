2d4256fa5f5a231c3b5e5d677fb40f8544773695
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-08-22 16:30:54 +0300

    sets expl

---
+++ JS/1/05-sets/script.js
@@ -0,0 +1,17 @@
+//sets
+// const set = new Set([2, 3, 4, 4, 1, 2, 3, 4]); // unique sets , not ordered
+
+// set.add(5);
+// console.log(set.has(3));
+// console.log(set.has(6));
+// set.delete(5);
+// set.clear();
+// console.log(set);
+// const setArray = Array.from(set);
+// console.log(setArray);
+
+// for (let item of set) {
+// 	console.log(item);
+// }
+// const iterator = set.values();
+// console.log(iterator.next().value);
