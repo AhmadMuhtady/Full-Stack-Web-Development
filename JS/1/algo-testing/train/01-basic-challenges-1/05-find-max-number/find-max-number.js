622dde491745897f21acf1463423b4de905cc5fa
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-09-06 21:10:26 +0300

    find Max Number

--- JS/1/algo-testing/train/01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ JS/1/algo-testing/train/01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,12 @@
-function findMaxNumber() {}
-
+function findMaxNumber(arr) {
+	// return Math.max(...arr);
+	let max = arr[0];
+	for (let i = 1; i < arr.length; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+	}
+	return max;
+}
+console.log(findMaxNumber([1, 5, 3, 9, 2]));
 module.exports = findMaxNumber;
