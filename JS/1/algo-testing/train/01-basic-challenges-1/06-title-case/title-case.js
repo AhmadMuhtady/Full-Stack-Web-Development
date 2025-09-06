980e63486cb8ca471c36b6040bf2d5d08aaf1ca0
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-09-06 21:44:42 +0300

    title Case function

--- JS/1/algo-testing/train/01-basic-challenges-1/06-title-case/title-case.js
+++ JS/1/algo-testing/train/01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,11 @@
-function titleCase() {}
+function titleCase(str) {
+	const words = str.toLowerCase().split(' ');
 
+	for (let i = 0; i < words.length; i++) {
+		words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+	}
+
+	return words.join(' ');
+}
+console.log(titleCase('heLlo world'));
 module.exports = titleCase;
