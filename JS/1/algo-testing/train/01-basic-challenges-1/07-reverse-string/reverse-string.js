3a4376fb3393cc550f5f2acec46e79d746b9a5fc
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-09-06 22:21:03 +0300

    reverseString function  second

--- JS/1/algo-testing/train/01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ JS/1/algo-testing/train/01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,11 +1,17 @@
 function reverseString(str) {
-	let words = str.split(' ');
+	// let words = str.split(' ');
 
-	for (let i = 0; i < words.length; i++) {
-		words[i] = words[i].split('').reverse().join('');
+	// for (let i = 0; i < words.length; i++) {
+	// 	words[i] = words[i].split('').reverse().join('');
+	// }
+	// return words.join(' ');
+
+	let reversed = '';
+	for (let i = str.length - 1; i >= 0; i--) {
+		reversed += str[i];
 	}
-	return words.join(' ');
+	return reversed;
 }
 
-console.log(reverseString('Hello World'));
+console.log(reverseString('Hello'));
 module.exports = reverseString;
