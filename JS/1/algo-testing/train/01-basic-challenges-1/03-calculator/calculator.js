41da7860dc7588a147e6acb3468789888186387a
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-09-06 18:42:28 +0300

    calculator function

--- JS/1/algo-testing/train/01-basic-challenges-1/03-calculator/calculator.js
+++ JS/1/algo-testing/train/01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,31 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+	// if (operator === '+') {
+	// 	return num1 + num2;
+	// } else if (operator === '-') {
+	// 	return num1 - num2;
+	// } else if (operator === '/') {
+	// 	return num1 / num2;
+	// } else if (operator === '*') {
+	// 	return num1 * num2;
+	// } else if (operator === '%') {
+	// 	return num1 % num2;
+	// } else {
+	// 	return 'Please insert operator ["+", "-", "*", "/", "%"]';
+	// }
+	switch (operator) {
+		case '+':
+			return num1 + num2;
+		case '-':
+			return num1 - num2;
+		case '/':
+			return num1 / num2;
+		case '*':
+			return num1 * num2;
+		case '%':
+			return num1 % num2;
+		default:
+			throw Error('Invalid operator. Please insert ["+", "-", "*", "/", "%"]');
+	}
+}
 
 module.exports = calculator;
