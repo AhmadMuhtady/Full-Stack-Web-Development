24ac19e78c24eaa56b633b71abe4edb20dbcff54
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-09-06 20:09:26 +0300

    count occurrences function

--- JS/1/algo-testing/train/01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ JS/1/algo-testing/train/01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,14 @@
-function countOccurrences() {}
+function countOccurrences(str, letter) {
+	// let count = 0;
+	// for (let i = 0; i < str.length; i++) {
+	// 	if (str[i] === letter) {
+	// 		count++;
+	// 	}
+	// }
+	// return count;
 
+	return str.split(letter).length - 1;
+}
+
+console.log(countOccurrences('hello', 'l'));
 module.exports = countOccurrences;
