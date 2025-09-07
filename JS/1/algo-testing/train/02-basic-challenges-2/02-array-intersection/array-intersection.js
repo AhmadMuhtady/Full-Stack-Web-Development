8db1ec7198dcf4e8f4fd5d71e825a177283a5b88
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-09-07 19:54:29 +0300

    array Intersection function

--- JS/1/algo-testing/train/02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ JS/1/algo-testing/train/02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,20 @@
-function arrayIntersection() {}
-
+function arrayIntersection(arr1, arr2) {
+	// const arr = [];
+	// for (let i = 0; i < arr2.length; i++) {
+	// 	if (arr1.includes(arr2[i]) && !arr.includes(arr2[i])) {
+	// 		arr.push(arr2[i]);
+	// 	}
+	// }
+	// return arr;
+	const set1 = new Set(arr1);
+	const set2 = new Set(arr2);
+	const intersectionArray = [];
+	for (let num of set2) {
+		if (set1.has(num)) {
+			intersectionArray.push(num);
+		}
+	}
+	return intersectionArray;
+}
+console.log(arrayIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]));
 module.exports = arrayIntersection;
