18790e924bd7b0e3530d0c6c6f8d43ed264b47f3
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-09-07 18:51:15 +0300

    fizzBuzz Game function

--- JS/1/algo-testing/train/02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ JS/1/algo-testing/train/02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,19 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+	let fizzBuzzArray = [];
+	for (let i = 1; i <= num; i++) {
+		if (i % 15 === 0) {
+			//(i % 3 === 0 && i % 5 === 0)
+			fizzBuzzArray.push('FizzBuzz');
+		} else if (i % 3 === 0) {
+			fizzBuzzArray.push('Fizz');
+		} else if (i % 5 === 0) {
+			fizzBuzzArray.push('Buzz');
+		} else {
+			fizzBuzzArray.push(i);
+		}
+	}
+	return fizzBuzzArray;
+}
 
+console.log(fizzBuzzArray(15));
 module.exports = fizzBuzzArray;
