cf04d4df87a494b2d4d8b5d50e9fdc1a8fd85753
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-09-07 21:22:38 +0300

    find Missing Letter function

--- JS/1/algo-testing/train/02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ JS/1/algo-testing/train/02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,24 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+	// const alphabets = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	// const startIndex = alphabets.indexOf(arr[0]);
+	// for (let i = 0; i < arr.length; i++) {
+	// 	if (arr[i] !== alphabets[startIndex + i]) {
+	// 		return alphabets[startIndex + i];
+	// 	}
+	// }
+	// return '';
+
+	let start = arr[0].charCodeAt(0);
+	for (let i = 0; i < arr.length; i++) {
+		const current = arr[i].charCodeAt(0);
+		if (current - start > 1) {
+			return String.fromCharCode(start + 1);
+		}
+
+		start = current;
+	}
+
+	return '';
+}
 
 module.exports = findMissingLetter;
