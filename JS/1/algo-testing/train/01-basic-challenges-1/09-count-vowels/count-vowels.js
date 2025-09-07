8ded96f588e485fa6d1881f355bc3cfb258cff55
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-09-07 17:43:40 +0300

    remove Duplicates from array function

--- JS/1/algo-testing/train/01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ JS/1/algo-testing/train/01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,29 +1,29 @@
 function countVowels(str) {
-	// let count = 0;
-	// const arr = str.toLowerCase().split('');
-	// const vowels = ['e', 'a', 'o', 'u', 'i'];
-	// for (let i = 0; i <= arr.length - 1; i++) {
-	// 	if (vowels.includes(arr[i])) {
-	// 		count++;
-	// 	}
-	// }
-	// return count;
-	const formattedStr = str.toLowerCase();
 	let count = 0;
-
-	for (let i = 0; i < formattedStr.length; i++) {
-		let char = formattedStr[i];
-		if (
-			char === 'a' ||
-			char === 'e' ||
-			char === 'u' ||
-			char === 'o' ||
-			char === 'i'
-		) {
+	const arr = str.toLowerCase().split('');
+	const vowels = ['e', 'a', 'o', 'u', 'i'];
+	for (let i = 0; i < arr.length; i++) {
+		if (vowels.includes(arr[i])) {
 			count++;
 		}
 	}
 	return count;
+	// const formattedStr = str.toLowerCase();
+	// let count = 0;
+
+	// for (let i = 0; i < formattedStr.length; i++) {
+	// 	let char = formattedStr[i];
+	// 	if (
+	// 		char === 'a' ||
+	// 		char === 'e' ||
+	// 		char === 'u' ||
+	// 		char === 'o' ||
+	// 		char === 'i'
+	// 	) {
+	// 		count++;
+	// 	}
+	// }
+	// return count;
 }
 
 console.log(countVowels('JavaScript'));
--- JS/1/algo-testing/train/01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ JS/1/algo-testing/train/01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,13 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+	return Array.from(new Set(arr));
 
+	const newArray = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (!newArray.includes(arr[i])) {
+			newArray.push(arr[i]);
+		}
+	}
+	return newArray;
+}
+console.log(removeDuplicates([true, true, false, true, false]));
 module.exports = removeDuplicates;
