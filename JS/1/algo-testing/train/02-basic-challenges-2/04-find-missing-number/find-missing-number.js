8db9e3b2f263bab3054ec8255762cde445134c31
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-09-07 20:57:49 +0300

    find Missing Number function

--- JS/1/algo-testing/train/02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ JS/1/algo-testing/train/02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,13 @@
-function findMissingNumber() {}
-
+function findMissingNumber(arr) {
+	const n = arr.length + 1;
+	const expectedSum = (n * (n + 1)) / 2;
+	// let actualSum = 0;
+	// if (arr.length === 0) return (count = 1);
+	// for (let i = 0; i < arr.length; i++) {
+	// 	actualSum += arr[i];
+	// }
+	const actualSum = arr.reduce((sum, num) => sum + num, 0);
+	return expectedSum - actualSum;
+}
+console.log(findMissingNumber([1, 2, 3, 5]));
 module.exports = findMissingNumber;
