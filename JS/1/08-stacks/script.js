040834624241fb8a824302eae07a143d82354336
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-09-09 01:17:00 +0300

    Queue

--- JS/1/08-stacks/script.js
+++ JS/1/08-stacks/script.js
@@ -1,49 +1,49 @@
-// class Stack {
-// 	constructor() {
-// 		this._items = []; // where we keep our stack items
-// 		this._count = 0; // keeps track of how many items are in the stack
-// 	}
+class Stack {
+	constructor() {
+		this._items = []; // where we keep our stack items
+		this._count = 0; // keeps track of how many items are in the stack
+	}
 
-// 	push(item) {
-// 		// this._items.push(item);
-// 		this._items[this._count] = item; // put the new item at the top (current count index)
-// 		this._count++; // increase count by 1
-// 	}
+	push(item) {
+		// this._items.push(item);
+		this._items[this._count] = item; // put the new item at the top (current count index)
+		this._count++; // increase count by 1
+	}
 
-// 	pop() {
-// 		// this._items.pop();
-// 		if (this.isEmpty()) {
-// 			return 'Underflow'; // can't remove from empty stack
-// 		}
+	pop() {
+		// this._items.pop();
+		if (this.isEmpty()) {
+			return 'Underflow'; // can't remove from empty stack
+		}
 
-// 		const item = this._items[this._count - 1]; // get the last item
-// 		this._count--; // decrease count
+		const item = this._items[this._count - 1]; // get the last item
+		this._count--; // decrease count
 
-// 		this._items.length = this._count;
-// 		return item;
-// 	}
+		this._items.length = this._count;
+		return item;
+	}
 
-// 	peek() {
-// 		if (this.isEmpty()) {
-// 			return 'No item';
-// 		}
+	peek() {
+		if (this.isEmpty()) {
+			return 'No item';
+		}
 
-// 		return this._items[this._count - 1];
-// 	}
+		return this._items[this._count - 1];
+	}
 
-// 	isEmpty() {
-// 		return this._count === 0;
-// 	}
+	isEmpty() {
+		return this._count === 0;
+	}
 
-// 	length() {
-// 		return this._count;
-// 	}
+	length() {
+		return this._count;
+	}
 
-// 	clear() {
-// 		this._items = [];
-// 		this._count = 0;
-// 	}
-// }
+	clear() {
+		this._items = [];
+		this._count = 0;
+	}
+}
 
 // const stack = new Stack();
 // stack.push('item 1');
--- JS/1/algo-testing/train/07-stacks-queues-linked-lists/06-queue-implementation/queue.js
+++ JS/1/algo-testing/train/07-stacks-queues-linked-lists/06-queue-implementation/queue.js
@@ -1,3 +1,43 @@
-class Queue {}
+class Queue {
+	constructor() {
+		this.queue = [];
+		this.head = 0;
+		this.tail = 0;
+		this.maxSize = 100;
+	}
+
+	enqueue(item) {
+		if (this.isFull()) {
+			return false;
+		}
+
+		this.queue[this.tail] = item;
+		this.tail++;
+		return true;
+	}
+
+	dequeue() {
+		if (this.isEmpty()) return null; // safety check
+		const item = this.queue[this.head];
+		this.head++;
+		return item;
+	}
+
+	peek() {
+		return this.isEmpty() ? null : this.queue[this.head];
+	}
+
+	getLength() {
+		return this.tail - this.head;
+	}
+
+	isEmpty() {
+		return this.getLength() === 0;
+	}
+
+	isFull() {
+		return this.getLength() === this.maxSize;
+	}
+}
 
 module.exports = Queue;
