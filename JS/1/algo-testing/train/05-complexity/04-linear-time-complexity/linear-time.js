e5815af44137b08853767f3d0699648c33e6e56a
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-09-08 18:50:37 +0300

    Linear Time Complexity - O(n).

--- JS/1/algo-testing/train/05-complexity/04-linear-time-complexity/linear-time.js
+++ JS/1/algo-testing/train/05-complexity/04-linear-time-complexity/linear-time.js
@@ -3,3 +3,23 @@
 
   Linear time means that the time required to complete a function is directly proportional to the size of the input data set.
 */
+
+const sumArray = (arr) => {
+	let sum = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	}
+
+	return sum;
+};
+
+const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.time('Access Element 1');
+console.log(sumArray(arr1));
+console.timeEnd('Access Element 1');
+
+const arr2 = Array.from({ length: 10000000 }, (_, index) => index + 1);
+console.time('Access Element 2');
+console.log(sumArray(arr2));
+console.timeEnd('Access Element 2');
