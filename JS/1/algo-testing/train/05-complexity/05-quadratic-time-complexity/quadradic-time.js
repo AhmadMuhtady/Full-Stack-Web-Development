cc3fdca1a8766eff39ede49f17650375c61e4244
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-09-08 19:10:00 +0300

    Quadratic Time Complexity O(n^2)

--- JS/1/algo-testing/train/05-complexity/05-quadratic-time-complexity/quadradic-time.js
+++ JS/1/algo-testing/train/05-complexity/05-quadratic-time-complexity/quadradic-time.js
@@ -3,3 +3,27 @@ Quadradic Time O(n^2)
 
 Quadradic time means that the time required to complete a function is proportional to the square of the input data set.
 */
+
+const sumArray = (arr) => {
+	let sum1 = 0;
+	let sum2 = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		sum1 += arr[i];
+		for (let j = 0; j < arr.length; j++) {
+			sum2 += arr[j];
+		}
+	}
+
+	return sum1 + sum2;
+};
+
+const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.time('Access Element 1');
+console.log(sumArray(arr1));
+console.timeEnd('Access Element 1');
+
+const arr2 = Array.from({ length: 10000 }, (_, index) => index + 1);
+console.time('Access Element 2');
+console.log(sumArray(arr2));
+console.timeEnd('Access Element 2');
