dad7bf73e6e803f38425a9b06c8c13a53aafc538
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-09-09 01:40:03 +0300

    reverseStringWithQueue function

--- JS/1/algo-testing/train/07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
+++ JS/1/algo-testing/train/07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
@@ -1,5 +1,17 @@
 const Queue = require('./queue');
 
-const reverseStringWithQueue = (str) => {};
+const reverseStringWithQueue = (str) => {
+	let reverse = '';
+	const queue = new Queue();
+	for (let i = 0; i < str.length; i++) {
+		queue.enqueue(str[i]);
+	}
+
+	while (!queue.isEmpty()) {
+		reverse = queue.dequeue() + reverse;
+	}
+	return reverse;
+};
+console.log(reverseStringWithQueue('Hello'));
 
 module.exports = reverseStringWithQueue;
