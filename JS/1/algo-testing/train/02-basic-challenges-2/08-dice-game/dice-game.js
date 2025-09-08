4738477363ad92d5d46dcb7c6bff2c66867bf230
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-09-08 13:15:23 +0300

    dice Game Simulation function

--- JS/1/algo-testing/train/02-basic-challenges-2/08-dice-game/dice-game.js
+++ JS/1/algo-testing/train/02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,28 @@
-function diceGameSimulation() {}
+function diceGameSimulation(numSimulator) {
+	const rollDace = () => {
+		return Math.floor(Math.random() * 6) + 1;
+	};
+
+	const results = [];
+
+	for (let i = 0; i < numSimulator; i++) {
+		const dice1 = rollDace();
+		const dice2 = rollDace();
+		const sum = dice1 + dice2;
+
+		let result = '';
+		if (sum === 7 || sum === 11) {
+			result = 'win';
+		} else if (sum === 2 || sum === 3 || sum === 12) {
+			result = 'lose';
+		} else {
+			result = 'roll again';
+		}
+
+		results.push({ dice1, dice2, sum, result });
+	}
+
+	return results;
+}
 
 module.exports = diceGameSimulation;
