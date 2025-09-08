742040ed4b96a96eb008745e485642e993516fa7
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-09-09 01:58:25 +0300

    isPalindromeQueueStack function

--- JS/1/algo-testing/train/07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
+++ JS/1/algo-testing/train/07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
@@ -1,6 +1,25 @@
 const Queue = require('./queue');
 const Stack = require('./stack');
 
-function isPalindromeQueueStack() {}
+function isPalindromeQueueStack(str) {
+	const formatted = str.replace(/[^a-zA-z0-9]/g, '').toLowerCase();
+
+	const charQueue = new Queue();
+	const charStack = new Stack();
+
+	for (let i = 0; i < formatted.length; i++) {
+		const char = formatted.charAt(i);
+		charQueue.enqueue(char);
+		charStack.push(char);
+	}
+
+	while (!charQueue.isEmpty()) {
+		if (charQueue.dequeue() !== charStack.pop()) {
+			return false;
+		}
+	}
+
+	return true;
+}
 
 module.exports = isPalindromeQueueStack;
