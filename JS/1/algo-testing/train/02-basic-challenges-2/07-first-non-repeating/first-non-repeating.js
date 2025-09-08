6e50dce4bffc83df9fb7ec60409ac8c4e4dabd96
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-09-08 12:05:57 +0300

    find First NonRepeating Character function

--- JS/1/algo-testing/train/02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ JS/1/algo-testing/train/02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,35 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+	const str1 = str.replace(/\s/g, '').toLowerCase();
 
+	// // solution 1
+	// const charCount = {};
+
+	// for (const char of str1) {
+	// 	charCount[char] = (charCount[char] || 0) + 1;
+	// }
+
+	// for (const char of str1) {
+	// 	if (charCount[char] === 1) {
+	// 		return char;
+	// 	}
+	// }
+
+	// return null;
+
+	// solution 2 maps
+	const charCount = new Map();
+
+	for (const char of str1) {
+		charCount.set(char, (charCount.get(char) || 0) + 1);
+	}
+
+	for (const char of str1) {
+		if (charCount.get(char) === 1) {
+			return char;
+		}
+	}
+
+	return null;
+}
+console.log(findFirstNonRepeatingCharacter('hello world'));
 module.exports = findFirstNonRepeatingCharacter;
